830aa81d921d60874ddd4debfd8524aa634f72b5
Samuel <52568057+SamHJ@users.noreply.github.com>
2026-06-09 09:33:18 +0100

    update

--- src/pages/Waitlist.tsx
+++ src/pages/Waitlist.tsx
@@ -189,8 +189,8 @@ function Waitlist() {
       const selectedDate = new Date(value);
       const day = selectedDate.getDay();
   
-      // 3 = Wednesday, 4 = Thursday, 7 = Sunday
-      if (day === 3 || day === 4 || day === 7) {
+      // 3 = Wednesday, 4 = Thursday, 0 = Sunday
+      if (day === 3 || day === 4 || day === 0) {
         alert("Wednesdays, Thursdays and Sundays are not available.");
         return;
       }
